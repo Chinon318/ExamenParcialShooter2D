617bebfda4632a55d68c753332709b15773450fa
Gilberto Jair <giltryx618@gmail.com>
2025-04-27 13:33:52 -0600

    Initial Commit

---
+++ Assets/Scripts/LogicaDisparo.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class LogicaDisparo : MonoBehaviour
+{
+    public GameObject bulletPrefab; // Prefab de la bala
+    public GameObject[] bulletList;
+    private int bulletIndex = 0; // Índice de la bala actual
+    [SerializeField]private int cargador = 10;
+    public Transform firePoint; // Punto de disparo
+    public float bulletSpeed = 20f; // Velocidad de la bala
+
+    [SerializeField]private float tiempoEspera = 2f; // Tiempo de espera para desactivar la bala
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        bulletList = new GameObject[cargador]; // Inicializar la lista de balas
+        for(int i = 0; i < cargador; i++)
+        {
+            bulletList[i] = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            bulletList[i].SetActive(false); // Desactivar la bala al inicio
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Disparar();
+    }
+
+    private void Disparar()
+    {
+        if (Input.GetButtonDown("Fire1") && bulletList[bulletIndex].activeSelf == false)
+        {
+            bulletList[bulletIndex].SetActive(true); // Activar la bala
+            bulletList[bulletIndex].transform.position = firePoint.position; // Posicionar la bala en el punto de disparo
+            bulletList[bulletIndex].transform.rotation = firePoint.rotation; // Rotar la bala al disparar
+
+            Rigidbody2D rb = bulletList[bulletIndex].GetComponent<Rigidbody2D>();
+            rb.linearVelocity = firePoint.up * bulletSpeed; // Aplicar velocidad a la bala
+
+            bulletIndex = (bulletIndex + 1) % cargador; // Cambiar al siguiente índice de bala
+
+            StartCoroutine(DesactivarBala(tiempoEspera, bulletList[bulletIndex])); // Desactivar la bala después de 2 segundos
+        }
+    }
+
+
+    IEnumerator DesactivarBala(float tiempoEspera, GameObject bala)
+    {
+        yield return new WaitForSeconds(tiempoEspera); // Esperar el tiempo especificado
+        bala.SetActive(false); // Desactivar la bala después de esperar
+        bala.transform.position = firePoint.position; // Reiniciar la posición de la bala
+        bala.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero; // Reiniciar la velocidad de la bala
+    }
+
+}
---
+++ Assets/Scripts/LogicaJugador.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class LogicaJugador : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField]private float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        Movimiento();
+    }
+
+    private void Movimiento()
+    {
+        float horMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+
+        if (horMovement != 0 || verMovement != 0)
+        {
+            Vector2 movement = new Vector2(horMovement, verMovement).normalized * speed * Time.fixedDeltaTime;
+            rb.MovePosition(rb.position + movement);
+        }
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        rb.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(worldPos.y - rb.position.y, worldPos.x - rb.position.x) * Mathf.Rad2Deg - 90f);
+    }
+}
